4de72a3c858792626e70d191ab431d4bd1bd99a1
Seiji Tsukada <tukadas@funai.co.jp>
2020-11-13 13:57:31 +0900

    メニュー表示検討コードを追加

---
+++ iOS/test-menulist-001/test-menulist-001/MenuListView.swift
@@ -0,0 +1,15 @@
+//
+//  MenuListView.swift
+//  test-menulist-001
+//
+//  Created by 塚田誠司 on 2020/11/12.
+//  Copyright © 2020 say. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuListView:UIView {
+    @IBOutlet weak var mMenuItemCollection: UICollectionView!
+    
+}
---
+++ iOS/test-menulist-001/test-menulist-001/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test-menulist-001
+//
+//  Created by 塚田誠司 on 2020/11/12.
+//  Copyright © 2020 say. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("<ViewController::viewDidLoad>")
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+
+}
+
