067840b2e44a81a38ae9422c1cd008a409b7a0f5
Seiji Tsukada <tukadas@funai.co.jp>
2020-12-02 14:25:47 +0900

    編集画面おメニューリストの表示、非表示で左寄せできるかの検討コード追加

---
+++ iOS/Test-MenuList-004/Test-MenuList-004/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Test-MenuList-004
+//
+//  Created by 塚田誠司 on 2020/12/02.
+//  Copyright © 2020 say. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var btnview1: UIView!
+    @IBOutlet weak var btnview2: UIView!
+    @IBOutlet weak var btnview3: UIView!
+    @IBOutlet weak var btnview4: UIView!
+    @IBOutlet weak var btnview5: UIView!
+    @IBOutlet weak var btnview6: UIView!
+    @IBOutlet weak var btnview7: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        btnview5.isHidden = true
+    }
+
+
+}
+
