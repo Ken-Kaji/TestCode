be196a4e26c46fbe91078af5cebb3cc86b0edac1
Seiji Tsukada <tukadas@funai.co.jp>
2020-12-08 18:21:42 +0900

    [Test-DesignEditLayout-003] 動的ボタンアイコン表示検討

--- iOS/Test-DesignEditLayout-003/Test-DesignEditLayout-003/ViewController.swift
+++ iOS/Test-DesignEditLayout-003/Test-DesignEditLayout-003/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var MainTool1Button: UIButton!
+    @IBOutlet weak var MainTool2Button: UIButton!
+    @IBOutlet weak var MainTool3Button: UIButton!
+    @IBOutlet weak var MainTool4Button: UIButton!
+    @IBOutlet weak var MainToolBarView: UIView!
+    
+    var MainToolButtons:[UIButton] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        MainToolButtons.append(MainTool1Button)
+        MainToolButtons.append(MainTool2Button)
+        MainToolButtons.append(MainTool3Button)
+        MainToolButtons.append(MainTool4Button)
+        MainToolButtons[0].setImage(UIImage(named: "number3_1"), for: .normal)
+        MainTool2Button.setImage(UIImage(imageLiteralResourceName: "number3_2"), for: .normal)
+        MainToolButtons[3].setImage(UIImage(named: "number3_3"), for: .normal)
+
     }
 
 
